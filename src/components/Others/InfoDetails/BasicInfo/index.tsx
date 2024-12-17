1942c023514df20f431baf5c8d04dcece7f90024
Gabriel Alm <viniciusgsa99@gmail.com>
2024-12-16 21:57:27 -0300

    feat: adicionando capitalize no nome do analista.

--- src/components/Others/InfoDetails/BasicInfo/index.tsx
+++ src/components/Others/InfoDetails/BasicInfo/index.tsx
@@ -6,7 +6,7 @@ export default function BasicInfo({ info, title }: IProps) {
   return (
     <div className="rounded-lg border-[#1c2f4a] bg-[#132238] p-6">
       <h2 className="mb-4 text-base font-semibold lg:text-lg">{title}</h2>
-      <div className="mb-4 text-xl md:text-2xl">{info}</div>
+      <div className="mb-4 text-xl capitalize md:text-2xl">{info}</div>
     </div>
   );
 }
