69616023842c7e14db6a2fcbb4f33d14771d4348
ItaloAraujodev <italoarauju2019@gmail.com>
2025-01-06 23:59:44 -0300

    feat: implement NFT Artes and NFT Jogos components with data fetching and layout

--- src/app/nft-jogos/page.tsx
+++ src/app/nft-jogos/page.tsx
@@ -1,11 +1,12 @@
 import { LayoutPage } from "@/components/Layout";
+import { NFTJogosComponent } from "@/components/Pages/NFTJogos";
 import { Suspense } from "react";
 
 export default function NftJogos() {
   return (
-    <LayoutPage className="px-[5%] pt-10">
+    <LayoutPage className="px-6 pt-10">
       <Suspense>
-        <h1 className="text-white">NFT Jogos</h1>
+        <NFTJogosComponent />
       </Suspense>
     </LayoutPage>
   );
