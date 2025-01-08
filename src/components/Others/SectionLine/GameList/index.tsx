17523d1de73d52cf0f1fbf79f8e15c8278501c39
Gabriel Alm <viniciusgsa99@gmail.com>
2025-01-08 18:23:41 -0300

    fix: corrigindo title da linha duplicado.

--- src/components/Others/SectionLine/GameList/index.tsx
+++ src/components/Others/SectionLine/GameList/index.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { Common } from "@/components/Common";
 import CardGames from "../../CardGames";
 import CarouselPosts from "../../Carousels/Carousel";
 import SkeletonPost from "@/components/Skeletons/SkeletonPost";
 import type { IPost } from "@/interfaces/interfaces";
-import { Gamepad2 } from "lucide-react";
 
 interface IProps {
   posts: IPost[];
@@ -14,16 +12,11 @@ interface IProps {
 function GameList({ posts, isLoading }: IProps) {
   if (!posts?.length && !isLoading) {
     return (
-      <>
-        <Common.CommonTitleSection>
-          <Gamepad2 /> Explorador de Jogos
-        </Common.CommonTitleSection>
-        <div className="relative mx-auto flex h-48 w-full items-center justify-center">
-          <h1 className="text-3xl font-thin text-lootlab-font-highlight">
-            Nenhum jogo encontrado.
-          </h1>
-        </div>
-      </>
+      <div className="relative mx-auto flex h-48 w-full items-center justify-center">
+        <h1 className="text-3xl font-thin text-lootlab-font-highlight">
+          Nenhum jogo encontrado.
+        </h1>
+      </div>
     );
   }
 
