4b5c40bcb7c226a0af414eebeadea3476f12cc35
ItaloAraujodev <italoarauju2019@gmail.com>
2025-01-10 00:56:38 -0300

    feat: add new image domain and handle image loading errors in GameHeader component

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["ufape.com.br", "pub-3c35af89709b4cb295f1df369e7cd3fb.r2.dev"], // Adicione o domínio aqui
+    domains: [
+      "ufape.com.br",
+      "pub-3c35af89709b4cb295f1df369e7cd3fb.r2.dev",
+      "pub-5055a299478c41f1989f7f36205ad5ed.r2.dev",
+    ], // Adicione o domínio aqui
   },
 };
 
